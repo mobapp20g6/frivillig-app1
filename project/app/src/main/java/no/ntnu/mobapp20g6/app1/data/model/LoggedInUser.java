1d9098e509d9103a7ee21b73f9609c23eb14024e
Nils Jarle Haugen <nils+git@gaupne.net>
2020-11-11 13:14:25 +0100

    add login fragment

---
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/model/LoggedInUser.java
@@ -0,0 +1,23 @@
+package no.ntnu.mobapp20g6.app1.data.model;
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+public class LoggedInUser {
+
+    private String userId;
+    private String displayName;
+
+    public LoggedInUser(String userId, String displayName) {
+        this.userId = userId;
+        this.displayName = displayName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
