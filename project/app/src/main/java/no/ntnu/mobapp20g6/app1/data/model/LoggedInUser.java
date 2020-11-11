1d2f5f0c2aa0806d9676986b3f7f19ed39038cfd
Nils Jarle Haugen <nils+git@gaupne.net>
2020-11-11 15:12:04 +0100

    added api

--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/model/LoggedInUser.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/model/LoggedInUser.java
@@ -1,23 +1,42 @@
 package no.ntnu.mobapp20g6.app1.data.model;
 
+import com.auth0.android.jwt.JWT;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
 /**
  * Data class that captures user information for logged in users retrieved from LoginRepository
  */
-public class LoggedInUser {
+public class LoggedInUser extends User{
 
-    private String userId;
+    private JWT userToken;
     private String displayName;
 
-    public LoggedInUser(String userId, String displayName) {
-        this.userId = userId;
-        this.displayName = displayName;
+    public LoggedInUser() {
+        super();
     }
 
-    public String getUserId() {
-        return userId;
+    public String getUserToken() {
+        return userToken.toString();
     }
 
     public String getDisplayName() {
         return displayName;
     }
+    public void setUserToken(String token) {
+        this.userToken = new JWT(token);
+
+    }
+
+    public Date getExpireTime() {
+        return userToken.getExpiresAt();
+    }
+
+    public Boolean isTokenExpired() {
+        return userToken.isExpired(3600);
+    }
+
 }
