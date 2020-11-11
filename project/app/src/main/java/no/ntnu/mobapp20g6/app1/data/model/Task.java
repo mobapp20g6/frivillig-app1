a062e8db8e258715cd0b0f8708bca6b539a42ae8
Trym Vaaland <trymv@stud.ntnu.no>
2020-11-11 20:37:37 +0100

    Added Task model

--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/model/Task.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/model/Task.java
@@ -1,4 +1,104 @@
 package no.ntnu.mobapp20g6.app1.data.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
 public class Task {
+
+    @Expose
+    @SerializedName("id")
+    private Long id;
+
+    @Expose
+    @SerializedName("title")
+    private String title;
+
+    @Expose
+    @SerializedName("description")
+    private String description;
+
+    @Expose
+    @SerializedName("participantCount")
+    private Long participantCount;
+
+    @Expose
+    @SerializedName("participantLimit")
+    private Long participantLimit;
+
+    @Expose
+    @SerializedName("scheduleDate")
+    private Date scheduleDate;
+
+    @Expose
+    @SerializedName("created")
+    private Date created;
+
+    @Expose
+    @SerializedName("creatorUser")
+    private User creator;
+
+    @Expose
+    private Picture picture;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getParticipantCount() {
+        return participantCount;
+    }
+
+    public void setParticipantCount(Long participantCount) {
+        this.participantCount = participantCount;
+    }
+
+    public Long getParticipantLimit() {
+        return participantLimit;
+    }
+
+    public void setParticipantLimit(Long participantLimit) {
+        this.participantLimit = participantLimit;
+    }
+
+    public Date getScheduleDate() {
+        return scheduleDate;
+    }
+
+    public void setScheduleDate(Date scheduleDate) {
+        this.scheduleDate = scheduleDate;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public Picture getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Picture picture) {
+        this.picture = picture;
+    }
 }
