9d24a247381523922de8d13799fa56a72a175c5a
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-11-12 09:25:39 +0100

    Add Group and Location Models

---
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/model/Group.java
@@ -0,0 +1,44 @@
+package no.ntnu.mobapp20g6.app1.data.model;
+
+import java.util.Date;
+
+public class Group {
+
+    private Long groupId;
+
+    private String groupName;
+
+    private Date created;
+
+    private Long organizationID;
+
+    private String description;
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getGroupId() {
+        return groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public Long getOrganizationID() {
+        return organizationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/model/Location.java
@@ -0,0 +1,46 @@
+package no.ntnu.mobapp20g6.app1.data.model;
+
+public class Location {
+
+    private String locationID;
+
+    private String gpsLat;
+
+    private String gpsLong;
+
+    private String streetAddress;
+
+    private String city;
+
+    private Long postalCode;
+
+    private String country;
+
+    public String getLocationID() {
+        return locationID;
+    }
+
+    public String getGpsLat() {
+        return gpsLat;
+    }
+
+    public String getGpsLong() {
+        return gpsLong;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Long getPostalCode() {
+        return postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
