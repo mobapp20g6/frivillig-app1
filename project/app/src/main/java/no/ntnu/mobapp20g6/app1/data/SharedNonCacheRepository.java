07afab6077306dd142c0e309b9b6672683574bc1
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-11-14 12:06:33 +0100

    Add GroupRepo Methods.

--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/SharedNonCacheRepository.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/SharedNonCacheRepository.java
@@ -1,5 +1,11 @@
 package no.ntnu.mobapp20g6.app1.data;
 
+import java.util.List;
+import java.util.function.Consumer;
+
+import no.ntnu.mobapp20g6.app1.data.model.Group;
+import no.ntnu.mobapp20g6.app1.data.model.Task;
+
 /**
  * Requests information regarding Group and Location.
  * Does not hold caches of this information.
@@ -24,5 +30,39 @@ public class SharedNonCacheRepository {
         return instance;
     }
 
+    public void createGroup(String token, String title, Long orgId,
+                            Consumer<Result<Group>> createGroupCallBack) {
+        groupDataSource.createGroup(token, title, orgId, (createGroupResult) -> {
+            createGroupCallBack.accept(createGroupResult);
+        });
+    }
+
+    public void updateGroup(String token, String title, String description, Long groupId,
+                            Consumer<Result<Group>> updateGroupCallBack) {
+        groupDataSource.updateGroup(token, title, description, groupId, (updateGroupResult) -> {
+            updateGroupCallBack.accept(updateGroupResult);
+        });
+    }
+
+    public void addUserToGroup(String token, String userId, Long groupId,
+                               Consumer<Result<Boolean>> addUserToGroupCallBack) {
+        groupDataSource.addUserToGroup(token, userId, groupId, (adduserToGroupResult) -> {
+            addUserToGroupCallBack.accept(adduserToGroupResult);
+        });
+    }
+
+    public void getAllGroupTasks(String token, Long groupId,
+                                 Consumer<Result<List<Task>>> getAllGroupTasksCallBack) {
+        groupDataSource.getAllGroupTasks(token, groupId, (getAllGroupTasksResult) -> {
+            getAllGroupTasksCallBack.accept(getAllGroupTasksResult);
+        });
+    }
+
+    public void isOwnerOfGroup(String token, Long groupId,
+                               Consumer<Result<Boolean>> isOwnerOfGroupCallBack) {
+        groupDataSource.isOwnerOfGroup(token, groupId, (isOwnerOfGroupResult) -> {
+            isOwnerOfGroupCallBack.accept(isOwnerOfGroupResult);
+        });
+    }
 
 }
