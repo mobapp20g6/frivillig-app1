9643f8c2bbd7a627984ae71a48c85404a9b6e209
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-11-14 15:53:07 +0100

    Add CreateGroup Fragment, ViewModel and xml
    Added
    - Create Group text.
    - title and title input field
    - description and description input field.

---
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/CreateGroup.java
@@ -0,0 +1,38 @@
+package no.ntnu.mobapp20g6.app1.data;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import no.ntnu.mobapp20g6.app1.R;
+
+public class CreateGroup extends Fragment {
+
+    private CreateGroupViewModel mViewModel;
+
+    public static CreateGroup newInstance() {
+        return new CreateGroup();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.create_group_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CreateGroupViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/data/CreateGroupViewModel.java
@@ -0,0 +1,7 @@
+package no.ntnu.mobapp20g6.app1.data;
+
+import androidx.lifecycle.ViewModel;
+
+public class CreateGroupViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
