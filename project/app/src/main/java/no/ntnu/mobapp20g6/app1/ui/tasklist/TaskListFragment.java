1cf29528bd68dbc9d4712c9953ab1aeb698e93a8
Trym Vaaland <trymv@stud.ntnu.no>
2020-11-15 17:40:56 +0100

    Finished adding Task list fragment (Not testet yet)

--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/tasklist/TaskListFragment.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/tasklist/TaskListFragment.java
@@ -2,37 +2,111 @@ package no.ntnu.mobapp20g6.app1.ui.tasklist;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 import no.ntnu.mobapp20g6.app1.R;
+import no.ntnu.mobapp20g6.app1.data.model.Task;
 
 public class TaskListFragment extends Fragment {
 
-    private TaskListViewModel mViewModel;
-
-    public static TaskListFragment newInstance() {
-        return new TaskListFragment();
-    }
+    private TaskListViewModel taskListViewModel;
+    private TaskListViewAdapter taskListViewAdapter;
+    //TODO add field for TaskViewModel
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.task_list_fragment, container, false);
-    }
+        //To know what list recyclerView shall show.
+        Bundle args = getArguments();
+        String listToShow = args.getString("listToShow", "public");
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(TaskListViewModel.class);
-        // TODO: Use the ViewModel
-    }
+        NavController navController = NavHostFragment.findNavController(getParentFragment());
+        View root = inflater.inflate(R.layout.task_list_fragment, container, false);
+        taskListViewModel = new ViewModelProvider(this, new TaskListViewModelFactory()).get(TaskListViewModel.class);
+        taskListViewAdapter = new TaskListViewAdapter(new ArrayList<Task>(), onClick -> {
+            //TODO Implement functionality when opening a task.
+        });
 
+        final FloatingActionButton newTaskFab = root.findViewById(R.id.task_list_fab);
+        if(taskListViewModel.isLoggedIn()) {
+            newTaskFab.setVisibility(View.VISIBLE);
+            newTaskFab.setOnClickListener(onClick -> {
+                //TODO Implement functionality when creating a task. Use navController to go to new fragment.
+            });
+        }
+
+        final RecyclerView recyclerView = root.findViewById(R.id.recycler_view_task_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(taskListViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        switch(listToShow) {
+
+            case "public":
+                taskListViewModel.loadPublicTasks(listResult -> {
+                    //Snackbar is a small text view.
+                    Snackbar listResultMsg = Snackbar.make(root, "Unable to load public tasks.", Snackbar.LENGTH_LONG);
+                    listResultMsg.setTextColor(Color.YELLOW);
+                    if(listResult == null) {
+                        listResultMsg.show();
+                    } else {
+                        listResultMsg.dismiss();
+                    }
+                });
+                break;
+
+            case "own":
+                taskListViewModel.loadOwnTasks(listResult -> {
+                    //Snackbar is a small text view.
+                    Snackbar listResultMsg = Snackbar.make(root, "Unable to load own tasks.", Snackbar.LENGTH_LONG);
+                    listResultMsg.setTextColor(Color.YELLOW);
+                    if(listResult == null) {
+                        listResultMsg.show();
+                    } else {
+                        listResultMsg.dismiss();
+                    }
+                });
+                break;
+
+            case "assigned":
+                taskListViewModel.loadAssignedTasks(listResult -> {
+                    //Snackbar is a small text view.
+                    Snackbar listResultMsg = Snackbar.make(root, "Unable to load assigned tasks.", Snackbar.LENGTH_LONG);
+                    listResultMsg.setTextColor(Color.YELLOW);
+                    if(listResult == null) {
+                        listResultMsg.show();
+                    } else {
+                        listResultMsg.dismiss();
+                    }
+                });
+                break;
+
+            default:
+                Snackbar listResultMsg = Snackbar.make(root, "Went into default case when trying to load tasks.", Snackbar.LENGTH_LONG);
+                listResultMsg.setTextColor(Color.YELLOW);
+                listResultMsg.show();
+        }
+
+        return root;
+    }
 }
--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/tasklist/TaskListViewModel.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/tasklist/TaskListViewModel.java
@@ -1,7 +1,78 @@
 package no.ntnu.mobapp20g6.app1.ui.tasklist;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+import java.util.function.Consumer;
+
+import no.ntnu.mobapp20g6.app1.data.Result;
+import no.ntnu.mobapp20g6.app1.data.model.Task;
+import no.ntnu.mobapp20g6.app1.data.repo.LoginRepository;
+import no.ntnu.mobapp20g6.app1.data.repo.TaskRepository;
+
 public class TaskListViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private final TaskRepository taskRepo;
+    private final LoginRepository loginRepo;
+
+    public TaskListViewModel(TaskRepository taskRepo, LoginRepository loginRepo) {
+        this.taskRepo = taskRepo;
+        this.loginRepo = loginRepo;
+    }
+
+    /**
+     * Tries to load public tasks from the server and return them into local data.
+     * @param loadResultCallback Return a callback
+     */
+    public void loadPublicTasks(Consumer<Result<List<Task>>> loadResultCallback) {
+        taskRepo.getPublicTasks(loginRepo.getToken(), loadResultCallback);
+    }
+
+    /**
+     * Tries to load own tasks from the server and return them into local data.
+     * @param loadResultCallback Return a callback
+     */
+    public void loadOwnTasks(Consumer<Result<List<Task>>> loadResultCallback) {
+        taskRepo.getOwnedTasks(loginRepo.getToken(), loadResultCallback);
+    }
+
+    /**
+     * Tries to load assigned tasks from the server and return them into local data.
+     * @param loadResultCallback Return a callback
+     */
+    public void loadAssignedTasks(Consumer<Result<List<Task>>> loadResultCallback) {
+        taskRepo.getAssignedTasks(loginRepo.getToken(), loadResultCallback);
+    }
+
+    /**
+     * Return public tasks from the local data.
+     * @return public tasks from the local data.
+     */
+    public LiveData<List<Task>> getPublicTasks() {
+        return taskRepo.getLiveDataTaskList();
+    }
+
+    /**
+     * Return own tasks from the local data.
+     * @return own tasks from the local data.
+     */
+    public LiveData<List<Task>> getOwnTasks() {
+        return taskRepo.getLiveDataOwnedTasks();
+    }
+
+    /**
+     * Return assigned tasks from the local data.
+     * @return assigned tasks from the local data.
+     */
+    public LiveData<List<Task>> getAssignedTasks() {
+        return taskRepo.getLiveDataAssignedTasks();
+    }
+
+    /**
+     * Return true if user is still logged in.
+     * @return true if user is still logged in.
+     */
+    public boolean isLoggedIn() {
+        return loginRepo.isLoggedIn();
+    }
 }
