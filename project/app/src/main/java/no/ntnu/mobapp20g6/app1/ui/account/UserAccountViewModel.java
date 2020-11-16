68b040176d15717523fb0c82666baea633186039
Nils Jarle Haugen <nils+git@gaupne.net>
2020-11-16 10:58:27 +0100

    small design tweakes centering

--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/account/UserAccountViewModel.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/account/UserAccountViewModel.java
@@ -2,6 +2,9 @@ package no.ntnu.mobapp20g6.app1.ui.account;
 
 import androidx.lifecycle.ViewModel;
 
+import no.ntnu.mobapp20g6.app1.data.model.LoggedInUser;
+
 public class UserAccountViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    private LiveData<LoggedInUser>
 }
