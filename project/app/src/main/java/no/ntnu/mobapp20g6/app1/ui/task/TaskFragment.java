95d80c8671fa0a109197c48508343cf09395d612
Trym <Trymv@stud.ntnu.no>
2020-11-19 14:37:28 +0100

    Added functionality to TaskFragment and TaskViewModel

--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/task/TaskFragment.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/task/TaskFragment.java
@@ -2,6 +2,8 @@ package no.ntnu.mobapp20g6.app1.ui.task;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,28 +13,136 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import no.ntnu.mobapp20g6.app1.R;
+import no.ntnu.mobapp20g6.app1.data.RestService;
+import no.ntnu.mobapp20g6.app1.data.api.PictureApi;
+import no.ntnu.mobapp20g6.app1.data.model.Task;
 
 public class TaskFragment extends Fragment {
 
-    private TaskViewModel mViewModel;
+    private TaskViewModel taskViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        taskViewModel = new ViewModelProvider(requireActivity(), new TaskViewModelFactory()).get(TaskViewModel.class);
+    }
 
     public static TaskFragment newInstance() {
         return new TaskFragment();
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_task, container, false);
+        View root = inflater.inflate(R.layout.fragment_task, container, false);
+        Picasso picasso = taskViewModel.loadPicasso(getContext());
+        Task currentActiveTask = taskViewModel.getActiveTaskLiveData().getValue();
+        View view = container.getRootView();
+
+        //Buttons
+        final Button buttonShowParticipants = root.findViewById(R.id.button_task_show_participants);
+        final Button buttonAddToCal = root.findViewById(R.id.button_task_add_to_cal);
+        final Button buttonJoinTask = root.findViewById(R.id.button_task_join);
+        final Button buttonUpdateTask = root.findViewById(R.id.button_task_update);
+        final Button buttonDeleteTask = root.findViewById(R.id.button_task_delete);
+
+        final ImageView taskImage = root.findViewById(R.id.task_image);
+        final TextView title = root.findViewById(R.id.task_title);
+        final TextView description = root.findViewById(R.id.task_description);
+        final TextView taskDate = root.findViewById(R.id.task_date);
+        final TextView participantCount = root.findViewById(R.id.task_participants);
+        final TextView taskGroup = root.findViewById(R.id.task_group);
+
+        //Disable update and delete button if user is not owner of task.
+        if(!taskViewModel.isUserOwnerOfTask()) {
+            buttonUpdateTask.setEnabled(false);
+            buttonDeleteTask.setEnabled(false);
+        }
+        //Disable join task button if user already is member of task and disable add to calendar button if user is not member of task.
+        if(taskViewModel.isUserMemberOfTask()) {
+            buttonJoinTask.setEnabled(false);
+        } else {
+            buttonAddToCal.setEnabled(false);
+        }
+
+        //Load the task image.
+        if(currentActiveTask.getPicture() != null) {
+            picasso.load(RestService.DOMAIN + PictureApi.PREFIX + "getimage?name=" + currentActiveTask.getPicture().getId() + "&width=" + "480").into(taskImage);
+        }
+
+        title.setText(currentActiveTask.getTitle());
+        participantCount.setText("Participants: " + currentActiveTask.getParticipantCount() + "/" + currentActiveTask.getParticipantLimit());
+        taskDate.setText("Date: " + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(currentActiveTask.getScheduleDate()));
+
+        //Sets task description if the task got one.
+        if(currentActiveTask.getDescription() != null || !currentActiveTask.getDescription().isEmpty()) {
+            description.setText(currentActiveTask.getDescription());
+        }
+
+        //Sets the name of group the task is associated with else it will be "Public"
+        if(currentActiveTask.getAssociatedGroup() != null) {
+            taskGroup.setText(currentActiveTask.getAssociatedGroup().getGroupName());
+        }
+
+        //Buttons
+        buttonShowParticipants.setOnClickListener(button ->{
+            //TODO add functionality to show all participants of task.
+        });
+        buttonAddToCal.setOnClickListener(button ->{
+            //TODO add functionality to add date to phone calendar.
+        });
+        buttonJoinTask.setOnClickListener(button ->{
+            if(!taskViewModel.isUserMemberOfTask()) {
+                taskViewModel.joinActiveTask(success-> {
+                    if(success) {
+                        buttonJoinTask.setEnabled(false);
+                        setSnackbarText("You successfully join the task!", view, true).show();
+                    } else {
+                        setSnackbarText("Joining task failed.", view, false);
+                    }
+                });
+            } else {
+                setSnackbarText("Joining task failed. You are already member of the task.", view, false);
+            }
+        });
+        buttonUpdateTask.setOnClickListener(button ->{
+            //TODO add functionality to update a task.
+        });
+        buttonDeleteTask.setOnClickListener(button ->{
+            //TODO add functionality to delete a task.
+        });
+        return root;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
-        // TODO: Use the ViewModel
+    /**
+     * Makes a small bar with text on the screen.
+     * @param msg text to be shown on the screen.
+     * @param view view the container is holding on.
+     * @param success true will show green text and false will show red text.
+     * @return Snackbar with text and yellow color.
+     */
+    private Snackbar setSnackbarText(String msg, View view, boolean success) {
+        if(view != null) {
+            Snackbar listResultMsg = Snackbar.make(view, msg, Snackbar.LENGTH_LONG);
+            if(success) {
+                listResultMsg.setTextColor(Color.GREEN);
+            } else {
+                listResultMsg.setTextColor(Color.RED);
+            }
+            return listResultMsg;
+        }
+        return null;
     }
-
 }
--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/task/TaskViewModel.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/ui/task/TaskViewModel.java
@@ -1,11 +1,22 @@
 package no.ntnu.mobapp20g6.app1.ui.task;
 
+import android.content.Context;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
+import java.util.function.Consumer;
+
+import no.ntnu.mobapp20g6.app1.data.Result;
 import no.ntnu.mobapp20g6.app1.data.model.Task;
+import no.ntnu.mobapp20g6.app1.data.model.User;
 import no.ntnu.mobapp20g6.app1.data.repo.LoginRepository;
 import no.ntnu.mobapp20g6.app1.data.repo.TaskRepository;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 public class TaskViewModel extends ViewModel {
     private TaskRepository taskRepository;
@@ -18,5 +29,98 @@ public class TaskViewModel extends ViewModel {
         if(activeTaskLiveData == null) {
             activeTaskLiveData = new MutableLiveData<>();
         }
+    }
+
+    /**
+     * Return true if user is still logged in.
+     * @return true if user is still logged in.
+     */
+    public boolean isLoggedIn() {
+        return loginRepository.isLoggedIn();
+    }
+
+    /**
+     * Return true if the current logged in user is the creator of the current viewed task.
+     * @return true if the current logged in user is the creator of the current viewed task.
+     */
+    public boolean isUserOwnerOfTask() {
+        return loginRepository.getCurrentUser().getUserId().equals(activeTaskLiveData.getValue().getCreator().getUserId());
+    }
+
+    /**
+     * Return true if user is a participant of the current task.
+     * @return true if user is a participant of the current task.
+     */
+    public boolean isUserMemberOfTask() {
+        for(User user: activeTaskLiveData.getValue().getParticipants()) {
+            if(user.getUserId().equals(loginRepository.getCurrentUser().getUserId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Return the current active task live data object.
+     * @return the current active task live data object.
+     */
+    public MutableLiveData<Task> getActiveTaskLiveData() {
+        return activeTaskLiveData;
+    }
+
+    /**
+     * Sets the current active task in TaskViewModel.
+     * @param task task to be set as current active.
+     */
+    public void setActiveTask(Task task) {
+        activeTaskLiveData.setValue(task);
+    }
+
+    private void loadActiveTask(Long id) {
+        taskRepository.getTask(loginRepository.getToken(), id, this::setActiveTask);
+    }
+
+    /**
+     * Builds the picasso with Authorization Bearer <token>.
+     * @param context current context of app.
+     * @return Picasso with build in Authorization Bearer <token>.
+     */
+    public Picasso loadPicasso(Context context) {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(chain -> {
+                    Request newRequest = chain.request().newBuilder()
+                            .addHeader("Authorization", loginRepository.getToken()).build();
+                    return chain.proceed(newRequest);
+                }).build();
+        return new Picasso.Builder(context).downloader(new OkHttp3Downloader(client)).build();
+    }
+
+    /**
+     * Add a user to the current active task if task is not full.
+     * @param joinTaskCallbackResult
+     */
+    public void joinActiveTask(Consumer<Boolean> joinTaskCallbackResult) {
+        Task task = activeTaskLiveData.getValue();
+        if(task != null) {
+            taskRepository.joinTask(loginRepository.getToken(), task.getId(), taskToJoin -> {
+                loadActiveTask(task.getId());
+                if(activeTaskLiveData.getValue() != null && !isTaskFull()) {
+                    //User successfully joined the task.
+                    joinTaskCallbackResult.accept(true);
+                } else {
+                    joinTaskCallbackResult.accept(false);
+                }
+            });
+        } else {
+            joinTaskCallbackResult.accept(false);
+        }
+    }
+
+    /**
+     * Return true if current active task is full.
+     * @return true if current active task is full.
+     */
+    public boolean isTaskFull() {
+        return activeTaskLiveData.getValue().getParticipantCount() >= activeTaskLiveData.getValue().getParticipantLimit();
     }
 }
