951131bf7c2be052e16acc6b481c961bee3a0fba
Trym <Trymv@stud.ntnu.no>
2020-11-30 16:15:17 +0100

    Login is now an Activity
    *Main screen when opening app is now Welcome Activity
    *Removed some unused Main Activity menu items

---
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package no.ntnu.mobapp20g6.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import no.ntnu.mobapp20g6.app1.ui.login.LoginActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_activity);
+
+        Button loginButton = findViewById(R.id.login_button);
+        Button registerButton = findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(onClick->{
+
+        });
+        loginButton.setOnClickListener(onClick->{
+            startActivity(new Intent(this, LoginActivity.class));
+        });
+    }
+}
