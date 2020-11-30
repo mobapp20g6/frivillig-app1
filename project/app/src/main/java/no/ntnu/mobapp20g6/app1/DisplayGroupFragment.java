c30dee9151bf08470478f1f9ef33d7319a2d07da
Martin Håhjem Årdal <martaard@stud.ntnu.no>
2020-11-30 18:25:11 +0100

    First Draft of Display Group UI

---
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/DisplayGroupFragment.java
@@ -0,0 +1,36 @@
+package no.ntnu.mobapp20g6.app1;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DisplayGroupFragment extends Fragment {
+
+    private DisplayGroupViewModel mViewModel;
+
+    public static DisplayGroupFragment newInstance() {
+        return new DisplayGroupFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_display_group, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(DisplayGroupViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
