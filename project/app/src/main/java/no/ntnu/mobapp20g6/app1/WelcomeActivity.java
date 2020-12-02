4eedc5227dea9286d2efe75e3ce75a4d223c5fa3
Trym <Trymv@stud.ntnu.no>
2020-12-02 10:27:41 +0100

    Removed unused import

--- project/app/src/main/java/no/ntnu/mobapp20g6/app1/WelcomeActivity.java
+++ project/app/src/main/java/no/ntnu/mobapp20g6/app1/WelcomeActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import no.ntnu.mobapp20g6.app1.ui.createaccount.CreateAccountActivity;
